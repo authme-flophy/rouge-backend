789008e81e77fd627df87b5c43a15ae644d334f1
waikau maina <ericmaina.dev@gmail.com>
2023-02-15 12:45:35 +0300

    created the route for carts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :products
   resources :categories
+  resources :carts
   mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
